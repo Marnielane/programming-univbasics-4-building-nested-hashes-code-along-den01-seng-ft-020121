ec89f4effa87ac8bf58ae9ed6906bc9cace582c9
Marnie Robinson <marnielane@gmail.com>
2021-01-12 23:47:21 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -2,13 +2,10 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"
-        #your key/value pairs here
         },
-      :matriarch => {
-        #your key/value pairs here
+      :matriarch => {name: "Lady Montague", age: "54"
         },
-      :hero => {
-        #your key/value pairs here
+      :hero => {name: "Romeo", age: "15", status: "alive"
         },
       :hero_friends => []
    }, 
